d568cbd2a171ca29b7c69244b493382ebf3219e7
ArslanAli394 <arslanmushtaq674@gmail.com>
2022-06-18 22:48:31 +0500

    changes

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ if(process.env.NODE_ENV =="production"){
 }
 
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(process.env.port || 5000, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => app.listen(process.env.PORT || 5000, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
 
 mongoose.set('useFindAndModify', false)
